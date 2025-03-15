7dd51362f885b276606929724e64b1a7d4281ae7
LeonelC98 <Leonel18costamagna@gmail.com>
2025-03-15 15:54:04 -0300

    Refactorizacion del componente Content, Creacion del componente part

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,14 @@ const Header =({course})=> <h1>{course}</h1>
 const Content = ({part1,part2,part3,exercises1,exercises2,exercises3})=>{
   return(
     <div>
-  <p>
-    {part1} {exercises1}
-  </p>
-  <p>
-    {part2} {exercises2}
-  </p>
-  <p>
-    {part3} {exercises3}
-  </p>
-</div>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </div>
 )}
 const Total = ({exercises1,exercises2,exercises3}) => <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 
-
+const Part =({part, exercises})=>   <p>{part} {exercises}</p>
 
 const App = () => {
   const course = 'Half Stack application development'
